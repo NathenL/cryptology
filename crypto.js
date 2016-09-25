525dfc3ac6693a27f39e578efe257d73ebd76563
Unknown <nathen@eurekaranch.com>
2016-09-25 00:54:53 -0400

    Added Multiplicative Cipher
    Can encrypt, decrypt, and use brute force

--- crypto.js
+++ crypto.js
@@ -17,7 +17,7 @@ cipher
   .option('-A, --Analyze', 'Analyze a Cipher. Depends on chosen Cipher Type.')
   .option('-s, --simple_substitution [value]', 'Simple Substitution')
   .option('-F, --Frequency_analyze [value]', 'Analyze letter frequency.')
-  .option('-m, --multiplicative', 'Multiplicative Cipher')
+  .option('-m, --multiplicative [value]', 'Multiplicative Cipher')
   .option('-a, --affine [value]', 'Affine Cipher')
   //.option('-k, --keyword [value]', 'Keyword Cipher')
   .option('-K, --Key [value]', 'Cipher Key')
@@ -30,20 +30,30 @@ var E = cipher.Encrypt;
 var D = cipher.Decrypt;
 var A = cipher.Analyze;
 
-if ((cipher.Encrypt && !cipher.Decrypt && !cipher.Analyze) ||
-  (!cipher.Encrypt && cipher.Decrypt && !cipher.Analyze) ||
-  (!cipher.Encrypt && !cipher.Decrypt && cipher.Analyze)) {
-
+if ((E && !D && !A) || (!E && D && !A) || (!E && !D && A))
+{
   // Simple Substitution
-  if (cipher.simple_substitution) {
+  if (cipher.simple_substitution)
+  {
     console.log('Simple Substitution...');
 
-    if (cipher.Encrypt) {
+    if (!cipher.Key && cipher.Decrypt)
+    {
+      console.log('No Key supplied, analyzing instead.');
+      cipher.Decrypt = false;
+      cipher.Analyze = true;
+    }
+
+    if (cipher.Encrypt)
+    {
       console.log('Method: ' + 'Encryption'.yellow);
-      if (!cipher.Key) {
+      if (!cipher.Key)
+      {
         console.log('(ERROR) You must supply a Substitution Alphabet as a KEY.'
           .red);
-      } else {
+      }
+      else
+      {
         //"QRCEJLAUDOXWFKVTIZNGMHPYBS"
         console.log("Substitution Alphabet: " + cipher.Key.toUpperCase().yellow);
         console.log("Plain-Text:  \"" + cipher.simple_substitution.yellow +
@@ -52,42 +62,78 @@ if ((cipher.Encrypt && !cipher.Decrypt && !cipher.Analyze) ||
         console.log("Cipher-Text: \"" + CT.yellow + "\"");
       }
     }
-
-    if (cipher.Decrypt) {
+    // Need to finish
+    else if (cipher.Decrypt)
+    {
       console.log('- Decryption'.yellow);
-      if (!cipher.Key) {
-        console.log('No Key supplied, analyzing instead.');
-        console.log('Method: ' + 'Frequency Analysis'.yellow);
-        var data = simple.analyze(cipher.simple_substitution);
+    }
+    else if (cipher.Analyze)
+    {
+      console.log('Method: ' + 'Frequency Analysis'.yellow);
+      var data = simple.analyze(cipher.simple_substitution);
 
-        for (var i = 0; i < 26; i++) {
-          letter = String.fromCharCode(97 + i);
-          count = data[letter];
+      for (var i = 0; i < 26; i++)
+      {
+        letter = String.fromCharCode(97 + i);
+        count = data[letter];
 
-          console.log(letter + "-".yellow + Array(count + 1).join("_".bgYellow.black) +
-            " " + count);
-        }
-      } else {
-
+        console.log(letter + "-".yellow + Array(count + 1).join("_".bgYellow.black) +
+          " " + count);
       }
     }
 
   }
   // --------------
 
-  if (cipher.multiplicative) {
-    if (cipher.Encrypt) {
-
-    } else if (cipher.Decrypt) {
-
-    } else {
-      console.log(
-        "(ERROR) Please choose to either --Encrypt, --Decrypt, or --Analyze."
-        .red);
+  if (cipher.multiplicative)
+  {
+    console.log('Multiplicative...');
+    if (cipher.Encrypt)
+    {
+      console.log('Method: ' + 'Encryption'.yellow);
+      if (!cipher.Key || [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25].indexOf(
+          cipher.Key % 26) == -1)
+      {
+        console.log(
+          '(ERROR) You must supply a valid multiplicative Key, MOD 26 (1,3,5,7,9,11,15,17,19,21,23,25).'
+          .red);
+      }
+      else
+      {
+        console.log("Key: " + cipher.Key.yellow);
+        console.log("Plain-Text:  \"" + cipher.multiplicative.yellow + "\"");
+        var CT = multi.encrypt(cipher.Key, cipher.multiplicative);
+        console.log("Cipher-Text: \"" + CT.yellow + "\"");
+      }
+    }
+    if (cipher.Decrypt)
+    {
+      console.log('Method: ' + 'Decryption'.yellow);
+      console.log("Key: " + ((cipher.Key) ? cipher.Key.yellow : "N/A".yellow));
+      console.log("Cipher-Text:  \"" + cipher.multiplicative.toUpperCase().yellow +
+        "\"");
+      var pt = multi.decrypt(cipher.Key, cipher.multiplicative);
+      if (pt instanceof Array)
+      {
+        console.log("Plain-Texts:");
+        pt.forEach(function(text, index)
+        {
+          console.log("" + text.yellow + "\n");
+        });
+      }
+      else
+      {
+        console.log("Plain-Text: \"" + pt.yellow + "\"");
+      }
     }
   }
-} else {
-  console.log('(ERROR) You must supply a Substitution Alphabet as a KEY.'.red);
+}
+else
+{
+  console.log(
+    '(ERROR) Please choose only to either --Encrypt, --Decrypt, or --Analyze.'
+    .red
+  );
 }
 console.log("\n\n------- END ------".blue.bgGreen);
 //END
